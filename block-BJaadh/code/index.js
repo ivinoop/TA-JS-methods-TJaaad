354eef0a15af4945c0f2d506f187b95cad921491
vinooprakash <vinooprakash93@gmail.com>
2021-04-27 18:40:46 +0530

    Array & String Methods Assignment 1 Block 2

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -12,41 +12,108 @@ Clone the array before using sort method: [...arr]
 
 // - Find the index of `101` in numbers
 
+numbers.indexOf(101);
+
 // - Find the last index of `9` in numbers
 
-// - Convert value of strings array into a sentance like "This is a collection of words"
+numbers.lastIndexOf(101);
 
-// - Add two new words in the strings array "called" and "sentance"
+// - Convert value of strings array into a sentence like "This is a collection of words"
 
-// - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
+strings.join(" ");
+
+// - Add two new words in the strings array "called" and "sentence"
+
+strings.push('called', 'sentence');
+
+// - Again convert the updated array (strings) into sentance like "This is a collection of words called sentence"
+
+strings.join(" ");
 
 // - Remove the first word in the array (strings)
 
+strings.shift();
+
 // - Find all the words that contain 'is' use string method 'includes'
+
+let stringIs = [];
+strings.forEach(str => {
+  if(str.includes('is')){
+    stringIs.push(str);
+  }
+});
+console.log(stringIs);
 
 // - Find all the words that contain 'is' use string method 'indexOf'
 
+let stringIs = [];
+strings.forEach(str => {
+  if(strings.indexOf(str) === 'is'){
+    stringIs.push(str);
+  }
+});
+console.log(stringIs);
+
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
+
+numbers.every(number => {
+  return number % 3 === 0;
+});
 
 // -  Sort Array from smallest to largest
 
+numbers.sort((a,b) => {
+  return a - b;
+});
+
 // - Remove the last word in strings
+
+strings.pop();
 
 // - Find largest number in numbers
 
+let largest = numbers.forEach((a,b) => {
+  if (a > b) {
+    return a;
+  } else return b;
+});
+
 // - Find longest string in strings
+
+strings.forEach(str => {
+  if(str[i].length > str[i+1].length) {
+    return str[i];
+  }  
+})
 
 // - Find all the even numbers
 
+numbers.filter(num => {
+  return num % 2 === 0;
+});
+
 // - Find all the odd numbers
+
+numbers.filter(num => {
+  return num % 2 !== 0;
+});
 
 // - Place a new word at the start of the array use (unshift)
 
+strings.unshift("newWord");
+
 // - Make a subset of numbers array [18,9,7,11]
+
+let newNumbers = [18,9,7,11];
+numbers.push(newNumbers);
 
 // - Make a subset of strings array ['a','collection']
 
+let newStrings = ['a','collection'];
+strings.push(newStrings);
+
 // - Replace 12 & 18 with 1221 and 1881
+
 
 // - Replace words in strings array with the length of the word
 
@@ -60,6 +127,10 @@ var customers = [
   { firstname: 'Jack', lastname: 'White' },
 ];
 // - Find all customers whose firstname starts with 'J'
+
+customers.firstname.forEach (function () {
+  if(customers.firstname.startsWith)
+})
 
 // - Create new array with only first name
 

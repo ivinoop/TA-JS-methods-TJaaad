e76a277b60c32721d9babcd59ae9529ba8eddf80
vinooprakash <vinooprakash93@gmail.com>
2021-04-27 19:21:18 +0530

    Refactor

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -36,33 +36,41 @@ strings.shift();
 
 // - Find all the words that contain 'is' use string method 'includes'
 
-let stringIs = [];
-strings.forEach(str => {
-  if(str.includes('is')){
-    stringIs.push(str);
-  }
-});
-console.log(stringIs);
+// let stringIs = [];
+// strings.forEach(str => {
+//   if(str.includes('is')){
+//     stringIs.push(str);
+//   }
+// });
+// console.log(stringIs);
+
+let allIs = strings.filter((string) =>
+  string.includes("is")
+);
 
 // - Find all the words that contain 'is' use string method 'indexOf'
 
-let stringIs = [];
-strings.forEach(str => {
-  if(strings.indexOf(str) === 'is'){
-    stringIs.push(str);
-  }
-});
-console.log(stringIs);
+// let stringIs = [];
+// strings.forEach(str => {
+//   if(strings.indexOf(str) === 'is'){
+//     stringIs.push(str);
+//   }
+// });
+// console.log(stringIs);
+
+let allIsNew = strings.filter((string) =>
+  string.indexOf("is") !== -1
+);
 
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
 
-numbers.every(number => {
-  return number % 3 === 0;
-});
+numbers.every((number) => 
+  number % 3 === 0
+);
 
 // -  Sort Array from smallest to largest
 
-numbers.sort((a,b) => {
+[...numbers].sort((a,b) => {
   return a - b;
 });
 
@@ -72,52 +80,59 @@ strings.pop();
 
 // - Find largest number in numbers
 
-let largest = numbers.forEach((a,b) => {
-  if (a > b) {
-    return a;
-  } else return b;
-});
+let largest = [...numbers].sort((a,b) => a - b).pop();
 
 // - Find longest string in strings
 
-strings.forEach(str => {
-  if(str[i].length > str[i+1].length) {
-    return str[i];
-  }  
-})
+let longestString = [...strings].sort((a,b) => a.length - b.length).pop();
 
 // - Find all the even numbers
 
-numbers.filter(num => {
+let even = numbers.filter(num => {
   return num % 2 === 0;
 });
 
 // - Find all the odd numbers
 
-numbers.filter(num => {
+let odd = numbers.filter(num => {
   return num % 2 !== 0;
 });
 
 // - Place a new word at the start of the array use (unshift)
 
-strings.unshift("newWord");
+[...strings].unshift("newWord");
 
 // - Make a subset of numbers array [18,9,7,11]
 
-let newNumbers = [18,9,7,11];
-numbers.push(newNumbers);
+console.log(numbers.slice(3,7));
 
 // - Make a subset of strings array ['a','collection']
 
-let newStrings = ['a','collection'];
-strings.push(newStrings);
+strings.slice(3,5);
 
 // - Replace 12 & 18 with 1221 and 1881
+
+console.log(numbers.map((num) => {
+  if(num === 12){
+    return 1221;
+  } else if(num === 18) {
+    return 1881;
+  } else {
+    return num;
+  }
+}));
 
 
 // - Replace words in strings array with the length of the word
 
+let stringsLength = strings.map((string) => string.length);
+
 // - Find the sum of the length of words using above question
+
+stringsLength.reduce( (acc, cv) => {
+  acc += cv;
+  return acc;
+}, 0)
 
 // - Customers Array
 var customers = [
@@ -128,14 +143,39 @@ var customers = [
 ];
 // - Find all customers whose firstname starts with 'J'
 
-customers.firstname.forEach (function () {
-  if(customers.firstname.startsWith)
-})
+let filteredCustomer = customers.filter((customer) => 
+  customer.firstname.startsWith("J")
+);
 
 // - Create new array with only first name
 
+let firstNameCustomer = customers.map((customer) => 
+  customer.firstname
+);
+
 // - Create new array with all the full names (ex: "Joe Blogs")
+
+let fullNameCustomer = customers.map((customer) => 
+  `${customer.firstname} ${cusomter.lastname}`
+);
 
 // - Sort the array created above alphabetically
 
+[...fullNameCustomer].sort();
+
 // - Create a new array that contains only user who has at least one vowel in the firstname.
+
+let vowelCustomer = customers.filter((cusomter) => {
+  if (
+    cusomter.firstname.toLowerCase().includes("a") || 
+    cusomter.firstname.toLowerCase().includes("e") || 
+    cusomter.firstname.toLowerCase().includes("i") || 
+    cusomter.firstname.toLowerCase().includes("o") || 
+    cusomter.firstname.toLowerCase().includes("u")  
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+);
